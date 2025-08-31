3f6e28ffda7074677ff213aa07d4c710396398d2
Conor Mullaney <conormullaney@Conors-MacBook-Pro.local>
2025-08-31 01:47:36 +0100

    initial parsing logic

--- src/argparse.cpp
+++ src/argparse.cpp
@@ -5,16 +5,24 @@ namespace clitools {
 
 // Parse raw argv into the parsed map
 ArgParser::ArgParser(int argc, char* argv[]) {
-  for (int i = 1; i < argc; ++i) {
-    std::string arg = argv[i];
-    if (arg.rfind("--", 0) == 0) {
-    if (i + 1 < argc && argv[i + 1][0] != '-') {
-        parsed[arg] = argv[++i];
-      } else {
-        parsed[arg] = "true";
-      }
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg.rfind(FLAG_PREFIX, 0) == 0) {
+            std::string value = "true";
+            
+            // --name Alice -> parsed["--name"] = "Alice"
+            // --debug      -> parsed["--debug"] = "true"
+            if (i + 1 < argc) { 
+                std::string next = argv[i + 1];
+                if (next.rfind(FLAG_PREFIX, 0) != 0) {
+                    value = next;
+                    ++i; 
+                }
+            }
+
+            parsed[arg] = value;
+        }
     }
-  }
 }
 
 void ArgParser::add_option(const std::string& flag, const std::string& desc) {
