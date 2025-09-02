788f223cf7f4f8642ebf204eefb3ea6f9eee3dbd
Conor Mullaney <conormullaney@Conors-MacBook-Pro.local>
2025-09-03 00:03:23 +0100

    lsp compile flags

--- src/argparse.cpp
+++ src/argparse.cpp
@@ -1,12 +1,11 @@
 #include "clitools/argparser.hpp"
 #include "clitools/constants.hpp"
-#include <string>
 
 namespace clitools {
 
 // Parse raw argv into the parsed map
-ArgParser::ArgParser(int argc, char* argv[]) {
-   for (int i = 1; i < argc; ++i) {
+ArgParser::ArgParser(int argc, char *argv[]) {
+  for (int i = 1; i < argc; ++i) {
     std::string arg = argv[i];
     if (arg.rfind(clitools::FLAG_PREFIX, 0) == 0) {
       std::string key = arg.substr(clitools::FLAG_PREFIX.size());
@@ -14,36 +13,37 @@ ArgParser::ArgParser(int argc, char* argv[]) {
 
       // --name Alice -> parsed["--name"] = "Alice"
       // --debug      -> parsed["--debug"] = "true"
-      if (i + 1 < argc) { 
+      if (i + 1 < argc) {
         std::string next = argv[i + 1];
         if (next.rfind(FLAG_PREFIX, 0) != 0) {
           value = next;
-          ++i; 
+          ++i;
         }
       }
-  parsed[key] = value;
+      parsed[key] = value;
     }
   }
 }
 
-void ArgParser::add_option(const std::string& flag, const std::string& desc) {
+void ArgParser::add_option(const std::string &flag, const std::string &desc) {
   options[flag] = desc;
 }
 
-bool ArgParser::has(const std::string& flag) const {
+bool ArgParser::has(const std::string &flag) const {
   return parsed.find(flag) != parsed.end();
 }
 
-std::string ArgParser::get(const std::string& flag, const std::string& def) const {
+std::string ArgParser::get(const std::string &flag,
+                           const std::string &def) const {
   auto it = parsed.find(flag);
   return it != parsed.end() ? it->second : def;
 }
 
 void ArgParser::help() const {
   std::cout << "Available options:\n";
-  for (const auto& [flag, desc] : options) {
-  std::cout << " " << flag << "\t" << desc << "\n";
+  for (const auto &[flag, desc] : options) {
+    std::cout << " " << flag << "\t" << desc << "\n";
   }
 }
 
-} // namespace clitools
+}  // namespace clitools
