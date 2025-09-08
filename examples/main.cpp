3a663045fafaaddfb44eb8a8191bf96c5780de7d
CMullaney01 <conormullreid@live.com>
2025-09-08 23:44:57 +0100

    update example

--- examples/main.cpp
+++ examples/main.cpp
@@ -14,39 +14,40 @@ using namespace clitools;
 // # 12
 
 int main(int argc, char* argv[]) {
-    // Root command
-    Command root("fctl", "Root command for clitools example");
+  // Root command
+  Command root("fctl", "Root command for clitools example");
 
-    // 'greet' subcommand
-    Command greet_cmd("greet", "Greet someone",
-        [](Command& cmd) {
-            // Get option --name, default to "World"
-            std::string name = cmd.get_option_or("name", "World");
-            std::cout << "Hello, " << name << "!\n";
-        });
+  // 'greet' subcommand
+  Command greet_cmd("greet", "Greet someone",
+    [](Command& cmd) {
+        // Get option --name, default to "World"
+        std::string name = cmd.get_option_or("name", "World");
+        std::cout << "Hello, " << name << "!\n";
+    });
+
 	greet_cmd.add_option("name", "Name to greet", "World");
 
-    // 'math' subcommand with nested 'add'
-    Command math_cmd("math", "Enables simple math operations");
-    Command add_cmd("add", "Add two numbers",
-        [](Command& cmd) {
-            auto a_str = cmd.require_option("a");
-            auto b_str = cmd.require_option("b");
+  // 'math' subcommand with nested 'add'
+  Command math_cmd("math", "Enables simple math operations");
+  Command add_cmd("add", "Add two numbers",
+    [](Command& cmd) {
+      auto a_str = cmd.require_option("a");
+      auto b_str = cmd.require_option("b");
 
-            int a = std::stoi(a_str);
-            int b = std::stoi(b_str);
-            std::cout << (a + b) << std::endl;
-        });
+      int a = std::stoi(a_str);
+      int b = std::stoi(b_str);
+      std::cout << (a + b) << std::endl;
+    });
 
-    add_cmd.add_option("a", "first number to add");
-	  // if we don't add b, it will not show up in help
+  add_cmd.add_option("a", "first number to add");
+  // if we don't add b, it will not show up in help
 	
-    math_cmd.add_command(add_cmd);
+  math_cmd.add_command(add_cmd);
 
-    // create a command with initial options
-    const std::vector<Command::Option> multiply_options = {
-		{.key = "x", .description = "first number to multiply", .default_value = "1"},
-		{.key = "y", .description = "second number to multiply", .default_value = "1"}
+  // create a command with initial options
+  const std::vector<Command::Option> multiply_options = {
+    {.key = "x", .description = "first number to multiply", .default_value = "1"},
+    {.key = "y", .description = "second number to multiply", .default_value = "1"}
 	};
 
 	Command multiply_cmd("multiply", "Multiply two numbers",
